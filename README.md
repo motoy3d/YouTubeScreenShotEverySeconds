532db21b63dbe5c4bc4cfb986f712e912f866bb0
motoy3d <motoy3d@gmail.com>
2023-05-23 15:04:59 +0900

    1秒おきにダウンロードするよう変更。320x180にリサイズするよう変更。

--- README.md
+++ README.md
@@ -1,21 +1,12 @@
-YouTube Screen Shot
+YouTube Screen Shot Every Seconds
 =====
 
-![YouTubeScreenShot](https://raw.githubusercontent.com/micelle/YouTubeScreenShot/master/assets/image.png)
-
-YouTubeのスクリーンショットを簡単に取れるChromeの拡張機能です。
+TODO URL
+YouTube動画のスクリーンショットを1秒おきに作成・ダウンロードできるChrome拡張機能です。
 
 ## ダウンロード
-https://chrome.google.com/webstore/detail/youtubescreenshot/kphdmkjcpejagaaijollpbmikeeehghi
+TODO URL
 
 ## 使い方
-1. 適当なYouTubeの動画を開きます。ライブ配信でもOKです。
-2. `📷`ボタンをクリックすることで、押したときに写っていた映像を「画像（PNGファイル）」としてダウンロードされます。わーい。  
-![download](https://raw.githubusercontent.com/micelle/YouTubeScreenShot/master/assets/download.gif)
-
-## 上手な使い方
-- 動画を一時停止させてから`📷`ボタンをクリックすることで、好みのスクショがダウンロードできるよ。
-- `<`ボタン・`>`ボタンをクリックすると、0.1秒ずつ動かせるよ。
-- `<<`ボタン・`>>`ボタンをクリックすると、1秒ずつ動かせるよ。
-- YouTubeの機能だけど<kbd>←</kbd>キーと<kbd>→</kbd>キーを押すと5秒ずつ動かせるんだって。  
-![current](https://raw.githubusercontent.com/micelle/YouTubeScreenShot/master/assets/current.gif)
+1. ChromeでYouTubeの動画を開きます。ライブ配信でもOKです。
+2. `📸`ボタンをクリックすることで、動画の最初から1秒おきに「画像（PNGファイル）」としてダウンロードできます。  
--- YouTubeScreenShot/src/script.js
+++ YouTubeScreenShot/src/script.js
@@ -1,82 +1,79 @@
+// スクリーンショットのサイズ TODO: オプション画面で設定できるとよい
+SCREENSHOT_WIDTH = 320
+SCREENSHOT_HEIGHT = 180
+// 最大秒数
+MAX_SECONDS = 180
+
 window.onload = () => {
-  // 読み込みが完了してないときがあるので…
+  // 読み込みが完了してないときがあるので2秒後に実行
   let timer = setInterval(() => {
     const path = location.pathname
     const trg = document.getElementById('player-theater-container')
     const ui = document.getElementById('screenshot-ui')
-    if (path === '/watch' && trg != null && ui == null) clearInterval(timer), setCurrentTimeHtml()
-  }, 1000)
+    if (path === '/watch' && trg != null && ui == null) {
+      clearInterval(timer)
+      setCurrentTimeHtml()
+    }
+  }, 2000)
 }
 
 const setCurrentTimeHtml = () => {
-  const nextButton1000 = document.createElement('button')
-  nextButton1000.id = 'currentTimeNext1000'
-  nextButton1000.textContent = '>>'
-  nextButton1000.title = '1秒進む'
-  nextButton1000.dataset.current = '1000'
-  const nextButton100 = document.createElement('button')
-  nextButton100.id = 'currentTimeNext100'
-  nextButton100.textContent = '>'
-  nextButton100.title = '0.1秒進む'
-  nextButton100.dataset.current = '100'
-  const prevButton100 = document.createElement('button')
-  prevButton100.id = 'currentTimePrev100'
-  prevButton100.textContent = '<'
-  prevButton100.title = '0.1秒戻る'
-  prevButton100.dataset.current = '-100'
-  const prevButton1000 = document.createElement('button')
-  prevButton1000.id = 'currentTimePrev1000'
-  prevButton1000.textContent = '<<'
-  prevButton1000.title = '1秒戻る'
-  prevButton1000.dataset.current = '-1000'
   const screenshotButton = document.createElement('button')
   screenshotButton.id = 'screenshot'
-  screenshotButton.textContent = '📷'
+  screenshotButton.textContent = '📸'
+  screenshotButton.title = 'スクリーンショット'
   const outer = document.createElement('div')
   const inner = document.createElement('div')
   outer.id = 'screenshot-ui'
-  inner.prepend(nextButton1000)
-  inner.prepend(nextButton100)
-  inner.prepend(prevButton100)
-  inner.prepend(prevButton1000)
   inner.prepend(screenshotButton)
   outer.prepend(inner)
   document.querySelector('.ytp-right-controls').prepend(outer)
   document.getElementById('screenshot-ui').onselectstart = () => false
-  document.getElementById('currentTimeNext1000').onclick = e => setCurrentTime(e)
-  document.getElementById('currentTimeNext100').onclick = e => setCurrentTime(e)
-  document.getElementById('currentTimePrev100').onclick = e => setCurrentTime(e)
-  document.getElementById('currentTimePrev1000').onclick = e => setCurrentTime(e)
-  document.getElementById('screenshot').onclick = () => getScreenshot()
+  document.getElementById('screenshot').onclick = () => {
+    (async () => {
+      const sleep = (second) => new Promise(resolve => setTimeout(resolve, second * 1000))
+      for (let i = 0; i < Math.min(getVideoDuration(), MAX_SECONDS); i++) {
+        setCurrentTime(i)
+        await sleep(2)
+        getScreenshot()
+        await sleep(1)
+      }
+    })()
+  }
 }
 
-const setCurrentTime = (e) => {
-  const time = Number(e.target.dataset.current) / 1000
+const getVideoDuration = () => {
   const video = document.querySelector('.video-stream')
-  const nowTime = video.currentTime
-  video.currentTime = nowTime + time
+  return video.duration
+}
+
+const setCurrentTime = (time) => {
+  const video = document.querySelector('.video-stream')
+  video.currentTime = time
 }
 
 const getScreenshot = () => {
   const zeroPadding = value => value < 10 ? String(`0${value}`) : String(value)
   const a = document.createElement('a')
-  const c = document.createElement('canvas')
-  const v = document.querySelector('.video-stream')
-  const t = (() => {
+  const canvas = document.createElement('canvas')
+  const video = document.querySelector('.video-stream')
+  const title = (() => {
     const e = document.querySelector('#container > h1')
     if (e !== null) return e.innerText
     return ''
   })()
-  const sa = ((v.currentTime % 60) % 60).toFixed(2).split('.')
+  const sa = ((video.currentTime % 60) % 60).toFixed(2).split('.')
   const ms = zeroPadding(Number(sa[1]))
   const s = zeroPadding(Number(sa[0]))
-  const m = zeroPadding(Math.floor((v.currentTime / 60) % 60))
-  const h = zeroPadding(Math.floor(v.currentTime / 3600))
-  a.download = `${t} ${h}-${m}-${s}.${ms}.png`
-  c.width = v.videoWidth
-  c.height = v.videoHeight
-  c.getContext('2d').drawImage(v, 0, 0)
-  c.toBlob((b) => {
+  const m = zeroPadding(Math.floor((video.currentTime / 60) % 60))
+  const h = zeroPadding(Math.floor(video.currentTime / 3600))
+  a.download = `${title} ${h}-${m}-${s}.${ms}.png`
+  canvas.width = SCREENSHOT_WIDTH
+  canvas.height = SCREENSHOT_HEIGHT
+  canvas.getContext('2d').drawImage(
+      video, 0, 0, video.videoWidth, video.videoHeight,
+      0, 0, SCREENSHOT_WIDTH, SCREENSHOT_HEIGHT)
+  canvas.toBlob((b) => {
     a.href = URL.createObjectURL(b)
     a.click()
   }, 'image/png')
